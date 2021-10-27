0751f9a75b5fc21a0c02d9b307d4e4bbd255d6c7
Hinux Chau <44890821+HinWaei@users.noreply.github.com>
2021-10-27 15:02:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -960,7 +960,7 @@ list() {
     esac
 }
 modify_camouflage_path() {
-    sed -i "/location/c \\\tlocation ${camouflage_path}" ${nginx_conf}
+    sed -i "/location/c \\\tlocation \/${camouflage_path}\/" ${nginx_conf}
     sed -i "/\"path\"/c \\\t  \"path\":\"${camouflage_path}\"" ${v2ray_conf}
     judge "V2ray camouflage path modified"
 }
