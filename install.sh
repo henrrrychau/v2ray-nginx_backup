716f00be19ca7b8b98ee1003bf648c6268e495bb
Henry Chau <44890821+henrrrychau@users.noreply.github.com>
2022-11-13 10:13:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget -N --no-check-certificate -q -O wulabing_install.sh "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh" && chmod +x wulabing_install.sh
 echo "0 3 * * * /sbin/reboot" >> /var/spool/cron/crontabs/root
 echo "*/20 * * * * /bin/sync && echo 3 > /proc/sys/vm/drop_caches" >> /var/spool/cron/crontabs/root
 echo "0 3 1 * * bash /bin/systemctl stop nginx && /root/.acme.sh/acme.sh --renew-all --force && /bin/systemctl start nginx" >> /var/spool/cron/crontabs/root
+wget -N --no-check-certificate -q -O wulabing_install.sh "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh" && chmod +x wulabing_install.sh && bash wulabing_install.sh
